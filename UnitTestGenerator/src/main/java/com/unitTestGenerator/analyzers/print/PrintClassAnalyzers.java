128729d647a8a82ef4f17aebabfc3228c28327b2
AixoraAroxia <AixoraAroxia@gmail.com>
2025-03-01 21:25:06 -0500

    implements and extends

--- UnitTestGenerator/src/main/java/com/unitTestGenerator/analyzers/print/PrintClassAnalyzers.java
+++ UnitTestGenerator/src/main/java/com/unitTestGenerator/analyzers/print/PrintClassAnalyzers.java
@@ -1,6 +1,9 @@
 package com.unitTestGenerator.analyzers.print;
 
 import com.unitTestGenerator.core.AppProjectStarted;
+import com.unitTestGenerator.pojos.Clase;
+import com.unitTestGenerator.pojos.Metodo;
+import com.unitTestGenerator.pojos.Project;
 
 public class PrintClassAnalyzers {
 
@@ -17,6 +20,31 @@ public class PrintClassAnalyzers {
     }
 
 
+    public void printProjectAnalize(Project projectP, boolean isAnalisis){
+
+        if(isAnalisis && projectP.getClaseList() != null) {
+            System.out.println("Classes found:...");
+            for (Clase clase : projectP.getClaseList()) {
+                if(clase.getMetodos() != null && !clase.getMetodos().isEmpty()){
+                    System.out.println(clase.getNombre() + "  package: " + clase.getPaquete());
+                    for(Metodo metod : clase.getMetodos()){
+                        if(metod.getInstanceMethodCalls() != null && !metod.getInstanceMethodCalls().isEmpty() ){
+                            metod.getInstanceMethodCalls().forEach(instanceMethodCall -> {
+                                System.out.println(  "CALL: -> Method: "+ metod.getNombre() + "| Call: "+instanceMethodCall.getOperation()+" |.");
+                            });
+                        }
+                    }
+                }
+            }
+        }
+
+        if(isAnalisis && projectP.getClaseList() != null) {
+            System.out.println("Classes found:");
+            for (Clase clase : projectP.getClaseList()) {
+                System.out.println(clase.getNombre() + "  package: " + clase.getPaquete());
+            }
+        }
+    }
 
 
 
--- UnitTestGenerator/src/main/java/com/unitTestGenerator/analyzers/services/AnalizeExtendsInInterfaces.java
+++ UnitTestGenerator/src/main/java/com/unitTestGenerator/analyzers/services/AnalizeExtendsInInterfaces.java
@@ -1,25 +1,61 @@
 package com.unitTestGenerator.analyzers.services;
 
 import com.unitTestGenerator.analyzers.print.DirectoryTreeBuilder;
+import com.unitTestGenerator.analyzers.print.PrintClassAnalyzers;
 import com.unitTestGenerator.pojos.Clase;
 import com.unitTestGenerator.pojos.ClassRelations;
 import com.unitTestGenerator.pojos.Project;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
-public interface AnalizeExtendsInInterfaces {
+public class AnalizeExtendsInInterfaces {
 
-    default String analizeExtendsofClassImplements( Project project, ClassRelations classRelations ){
+    private static AnalizeExtendsInInterfaces instance;
+
+    public static AnalizeExtendsInInterfaces getInstance(){
+        if(instance == null){
+            instance = new AnalizeExtendsInInterfaces();
+        }
+        return instance;
+    }
+
+    private  AnalizeExtendsInInterfaces() {
+    }
+
+    public String analizeImplements( Project project, Character key){
         StringBuilder sb = new StringBuilder();
-        for(String implemet : classRelations.getImplementsList()){
+
+
+            project.getClaseList().forEach(clase ->{
+                if (key.equals('I')){
+                    sb.append(analizeExtendsofClassImplements(project, clase.getClassRelations().getImplementsList()));
+                }else{
+                    if(clase.getClassRelations().getClassExtends() != null
+                            && !clase.getClassRelations().getClassExtends().equals("")
+                            && !clase.getClassRelations().getClassExtends().equals(" ")){
+                        List<String> elementsList = Arrays.asList(clase.getClassRelations().getClassExtends());
+                        sb.append(analizeExtendsofClassImplements(project,elementsList));
+                    }
+                }
+
+            });
+
+        return sb.toString();
+    }
+
+
+    private String analizeExtendsofClassImplements( Project project, List<String> elementsList ){
+        StringBuilder sb = new StringBuilder();
+        for(String implemet :  elementsList){
             sb.append("-- ").append(analizeExtendsInInterfaces(project, implemet)).append("\n");
         }
         return sb.toString();
     }
 
-    default String analizeExtendsInInterfaces(Project project, String classToAnalize){
+    private String analizeExtendsInInterfaces(Project project, String classToAnalize){
         List<String> tree = new ArrayList<>();
         Clase classToAnalizeMaterial = project.getClass(classToAnalize);
         ClassRelations relationsClassToAnalize = classToAnalizeMaterial.getClassRelations();
@@ -32,7 +68,7 @@ public interface AnalizeExtendsInInterfaces {
     }
 
 
-    default String getTreeString(List<String> tree,  String classToAnalize) {
+    private String getTreeString(List<String> tree,  String classToAnalize) {
         StringBuilder sb = new StringBuilder();
         for(String leaf : tree){
             if(classToAnalize.equals(leaf)){
