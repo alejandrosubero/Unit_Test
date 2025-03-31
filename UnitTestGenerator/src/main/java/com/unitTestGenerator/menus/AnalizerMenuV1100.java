b4cefc09b1ef9149c371ade70b52dbe07ce83fbc
AixoraAroxia <AixoraAroxia@gmail.com>
2025-03-31 18:23:58 -0400

    menu progress 1

--- UnitTestGenerator/src/main/java/com/unitTestGenerator/menus/AnalizerMenuV1100.java
+++ UnitTestGenerator/src/main/java/com/unitTestGenerator/menus/AnalizerMenuV1100.java
@@ -7,7 +7,9 @@ import com.unitTestGenerator.core.ProjectHolder;
 import com.unitTestGenerator.ioc.ContextIOC;
 import com.unitTestGenerator.ioc.anotations.Component;
 import com.unitTestGenerator.ioc.anotations.Singleton;
+import com.unitTestGenerator.pojos.Clase;
 import com.unitTestGenerator.pojos.Project;
+import com.unitTestGenerator.printers.PrintClassAnalyzers;
 import com.unitTestGenerator.printers.PrintProjectAnalyzers;
 import com.unitTestGenerator.test.AppTestDatabase;
 import com.unitTestGenerator.util.IBaseModel;
@@ -22,11 +24,15 @@ public class AnalizerMenuV1100  implements IAnalizerProjectServiceManager, IBase
 
     private ProjectHolder projectHolder;
     private AnalizerProjectService analizerProjectService;
+    private PrintClassAnalyzers printClassAnalyzers;
 
-    public AnalizerMenuV1100(ProjectHolder projectHolder, AnalizerProjectService analizerProjectService) {
+    public AnalizerMenuV1100(ProjectHolder projectHolder, AnalizerProjectService analizerProjectService, PrintClassAnalyzers printClassAnalyzers) {
         this.projectHolder = projectHolder;
         this.analizerProjectService = analizerProjectService;
+        this.printClassAnalyzers = printClassAnalyzers;
     }
+
+
 
     public void analizerMenu(){
         this.printColummString("Analyze project Module",
@@ -49,8 +55,8 @@ public class AnalizerMenuV1100  implements IAnalizerProjectServiceManager, IBase
                 Project projectsave = ContextIOC.getInstance(AppTestDatabase.class).getClassInstance(SavedProjectMenu.class).started(scanner);
 
                 if(projectsave == null){
-                    ...
-                }else {
+                    this.analizerMenu1( project,  scanner);
+                } else {
                     ...
                 }
 
@@ -64,6 +70,69 @@ public class AnalizerMenuV1100  implements IAnalizerProjectServiceManager, IBase
         }
     }
 
+    public void AnalysisOptionsMenu(){
+        this.printColummString(
+                "Choose an option:",
+                "1. Print the list of classes",
+                "2. Print the list of classes with methods",
+                "3. Print the project class tree",
+                "4. Print the project file tree",
+                "5. Print a class with details",
+                "6. Generate File",
+                "7. Return to the previous menu",
+                "8. Return to the main menu");
+    }
+
+
+
+    public void analizerMenuInitial(Project project){
+        Scanner scanner = new Scanner(System.in);
+        this.AnalysisOptionsMenu();
+        int opcion = scanner.nextInt();
+
+        switch (opcion) {
+            case 2:
+                methodsOfclass(scanner, project);
+                subMenu2( project,  scanner);
+                break;
+            case 3:
+                this.classDetail(scanner, project);
+                subMenu2( project,  scanner);
+                break;
+            case 4:
+                this.printProjectFileTree(project);
+                subMenu2( project,  scanner);
+                break;
+            case 5:
+                this.printProjectClassTree(project);
+                subMenu2( project,  scanner);
+                break;
+            case 6:
+                this.goToMainMenu();
+                break;
+            default:
+                this.printClassList(project);
+                this.subMenu1(project,scanner);
+        }
+    }
+
+    public void subMenu2V1000(){
+        this.printColummString(
+                "Choose an option:",
+                "1. Exit",
+                "2. Return to the previous menu",
+                "3. Return to the main menu");
+    }
+
+    public void subMenu1V1000(){
+        this.printColummString(
+                "Choose an option:",
+                "1. Print  methods of one class",
+                "2. Return to the previous menu",
+                "3. Return to the main menu");
+    }
+
+    
 
     private boolean askContinue(Scanner scanner) {
         System.out.println("Return to main menu? (y/n)");
@@ -81,19 +150,26 @@ public class AnalizerMenuV1100  implements IAnalizerProjectServiceManager, IBase
     }
 
 
-
-    public void AnalysisOptionsMenu(){
-        this.printColummString(
-                "Choose an option:",
-                "1. Print the list of classes",
-                "2. Print the list of classes with methods",
-                "3. Print the project class tree",
-                "4. Print the project file tree",
-                "5. Print a class with details",
-                "6. Generate File",
-                "7. Return to the previous menu",
-                "8. Return to the main menu");
+    private void methodsOfclass(Scanner scanner, Project project) {
+        System.out.println("Enter the name of the Class");
+        String response = scanner.next().toLowerCase();
+        if(response != null && !response.equals("")){
+            this.printMethodsOfClass(project, response);
+        }
     }
+
+    private void classDetail(Scanner scanner, Project project) {
+        System.out.println("Enter the name of the Class");
+        String response = scanner.next().toLowerCase();
+        if(response != null && !response.equals("")){
+            Clase classs = project.getClass(response);
+            if(classs != null && classs.getClassDetail() != null && !classs.getClassDetail().isEmpty()){
+                this.printClassAnalyzers.printClassDetail(classs);
+            }
+        }
+    }
+
+
 
 
 
